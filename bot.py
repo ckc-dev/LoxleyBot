e67b7bf468385134ad25bff8173faed75aa408fc
ckc <ckc-dev@pm.me>
2021-01-24 00:46:12 -0300

    Add Marco Polo.
    Users can now play Marco Polo with the bot.

--- bot.py
+++ bot.py
@@ -1,9 +1,15 @@
+"""
+Main bot file.
+"""
+
 # Import required modules.
 import logging
 import os
 
 import discord
 import dotenv
+
+import functions
 
 # Make sure bot token was provided.
 dotenv.load_dotenv()
@@ -14,17 +20,31 @@ if not TOKEN:
 # Set up logging.
 LOGGER = logging.getLogger('discord')
 HANDLER = logging.FileHandler(filename='bot.log', encoding='utf-8', mode='w')
-HANDLER.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+HANDLER.setFormatter(logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 LOGGER.setLevel(logging.DEBUG)
 LOGGER.addHandler(HANDLER)
 
-# Initialize a client instance.
+# Initialize a Client instance.
 CLIENT = discord.Client()
+
 
 # Once the bot is finished logging in and setting things up:
 @CLIENT.event
 async def on_ready():
     print("Hello, world!")
+
+
+# Every time a message is received:
+@CLIENT.event
+async def on_message(message):
+    # If message was sent by the bot, just return.
+    if message.author == CLIENT.user:
+        return
+
+    # If message matches some form of "Marco", play Marco Polo.
+    if functions.REGEX_MARCO.match(message.content):
+        await message.channel.send(functions.marco_polo(message.content))
 
 # Run bot.
 CLIENT.run(TOKEN)
